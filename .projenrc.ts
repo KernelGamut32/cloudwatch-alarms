01945813b7a76ac3229257de1f5e3ee81c73745a
Allen Sanders <asanders@gamuttechnologysvcs.com>
2024-04-08 08:10:13 -0400

    updated lab

--- .projenrc.ts
+++ .projenrc.ts
@@ -5,9 +5,9 @@ const project = new awscdk.AwsCdkTypeScriptApp({
   name: 'cloudwatch-alarms',
   projenrcTs: true,
 
-  // deps: [],             /* Runtime dependencies of this module. */
+  // deps: [],                /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
-  // devDeps: [],          /* Build dependencies for this module. */
+  // devDeps: [],             /* Build dependencies for this module. */
   // packageName: undefined,  /* The "name" in package.json. */
 
   context: {
